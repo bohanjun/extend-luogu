e253e6b970a652633bb309deeb82467b1f10424c
YuzhenQin <1@jin-dan.site>
2022-05-06 20:34:14 +0800

    [CI] Load tampermonkey in test/test.py

--- test/test.py
+++ test/test.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
-# coding: utf-8
+
+from playwright.sync_api import sync_playwright
+
+path_to_extension = "./test/tampermonkey"
+user_data_dir = "./tmp"
+
+
+def run(playwright):
+    context = playwright.chromium.launch_persistent_context(
+        user_data_dir,
+        headless=False,
+        args=[
+            f"--disable-extensions-except={path_to_extension}",
+            f"--load-extension={path_to_extension}",
+        ],
+    )
+    page = context.new_page()
+    page.goto("http://playwright.dev")
+    print(page.title())
+    context.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
